0af3a84812ed66d3e74b51591012bae801e89d5f
gelther <lpw.sormano@gmail.com>
2016-04-01 10:49:40 +0200

    Code consistency changes for includes/templates/template-metabox-generalpage.php

--- includes/templates/template-metabox-generalpage.php
+++ includes/templates/template-metabox-generalpage.php
@@ -1,16 +1,17 @@
 <div class="wswebinarStatusPanel">
-    <?php $saved_status = get_post_meta($post->ID, '_wswebinar_gener_webinar_status', true); ?>
+    <?php $saved_status = get_post_meta( $post->ID, '_wswebinar_gener_webinar_status', true ); ?>
     <ul>
-        <li class="<?php echo $this->decideClassOfStatusButtons("cou", $saved_status); ?>" status="cou">Countdown</li>
-        <li class="<?php echo $this->decideClassOfStatusButtons("liv", $saved_status); ?>" status="liv">Live</li>
-        <li class="<?php echo $this->decideClassOfStatusButtons("rep", $saved_status); ?>" status="rep">Replay</li>
-        <li class="<?php echo $this->decideClassOfStatusButtons("clo", $saved_status); ?>" status="clo">Closed</li>
-    </ul>            
+        <li class="<?php echo $this->decideClassOfStatusButtons( 'cou', $saved_status ); ?>" status="cou">Countdown</li>
+        <li class="<?php echo $this->decideClassOfStatusButtons( 'liv', $saved_status ); ?>" status="liv">Live</li>
+        <li class="<?php echo $this->decideClassOfStatusButtons( 'rep', $saved_status ); ?>" status="rep">Replay</li>
+        <li class="<?php echo $this->decideClassOfStatusButtons( 'clo', $saved_status ); ?>" status="clo">Closed</li>
+    </ul>
     <input type="hidden" name="gener_webinar_status" id="gener_webinar_status" value="<?php echo $saved_status; ?>">
     <script>
         jQuery(document).on('click', '.wswebinarStatusPanel li', function () {
-            if (jQuery(this).hasClass('disabled'))
+            if ( jQuery(this).hasClass('disabled') ) {
                 return;
+            }
             jQuery('.wswebinarStatusPanel li').removeClass('disabled active');
             jQuery(this).addClass('disabled active');
             jQuery('#gener_webinar_status').val(jQuery(this).attr('status'));
@@ -18,29 +19,29 @@
     </script>
 </div>
 <div class="form-field">
-    <label for="gener_date"><?php _e('Webinar starts at', WebinarSysteem::$lang_slug); ?></label>
-    <input type="text" name="gener_date" id="gener_date" placeholder="<?php _e('Date', WebinarSysteem::$lang_slug) ?>" value="<?php echo get_post_meta($post->ID, '_wswebinar_gener_date', true); ?>">
+    <label for="gener_date"><?php _e( 'Webinar starts at', WebinarSysteem::$lang_slug ); ?></label>
+    <input type="text" name="gener_date" id="gener_date" placeholder="<?php _e( 'Date', WebinarSysteem::$lang_slug ); ?>" value="<?php echo get_post_meta( $post->ID, '_wswebinar_gener_date', true ); ?>">
     <div class="date_line_sep">@</div>
-    <select class="alignleft" name="gener_hour">                
+    <select class="alignleft" name="gener_hour">
         <?php
-        for ($i = 0; $i < 24; $i++):
+        for ( $i = 0; $i < 24; $i++ ) :
             ?>
             <option <?php
-            $theHour = get_post_meta($post->ID, '_wswebinar_gener_hour', true);
-            echo!empty($theHour) && $theHour == $i ? 'selected="selected"' : ''
-            ?>><?php echo sprintf("%02s", $i) ?></option>
+            $theHour = get_post_meta( $post->ID, '_wswebinar_gener_hour', true );
+            echo! empty( $theHour ) && $theHour == $i ? 'selected="selected"' : '';
+            ?>><?php echo sprintf( '%02s', $i ); ?></option>
             <?php endfor; ?>
     </select>
     <div class="date_line_sep">:</div>
-    <select class="alignleft" name="gener_min">                
+    <select class="alignleft" name="gener_min">
         <?php
-        for ($j = 0; $j < 60; $j++):
+        for ( $j = 0; $j < 60; $j++ ) :
             ?>
-            <?php if ($j % 5 == 0): ?>
+            <?php if ( $j % 5 == 0 ) : ?>
                 <option <?php
-                $theMin = get_post_meta($post->ID, '_wswebinar_gener_min', true);
-                echo!empty($theMin) && $theMin == $j ? 'selected="selected"' : ''
-                ?>><?php echo sprintf("%02s", $j) ?></option>
+                $theMin = get_post_meta( $post->ID, '_wswebinar_gener_min', true );
+                echo! empty( $theMin ) && $theMin == $j ? 'selected="selected"' : '';
+                ?>><?php echo sprintf( '%02s', $j ); ?></option>
                 <?php endif; ?>
             <?php endfor; ?>
     </select>
@@ -53,34 +54,35 @@
                 dateFormat: "yy-mm-dd"
             });
         });
-    </script>            
+    </script>
     <div class="webinar_clear_fix"></div>
-</div>   
+</div>
 <div class="form-field">
-	<label>Timezone annotation</label>
-	<select name="timezoneidentifier">
+    <label>Timezone annotation</label>
+    <select name="timezoneidentifier">
     <option value="">Default WP</option>
     <?php
-	$timezoneidentifier=get_post_meta($post->ID, '_wswebinar_timezoneidentifier', true);
-	$getTimezoneIdentifiers=WebinarSysteem::getTimezoneIdentifiers();
-	foreach($getTimezoneIdentifiers as $timeZoneKey => $timeZoneLabel){
-		$selected = ($timezoneidentifier==$timeZoneKey) ? 'selected="selected"' : '';
-		echo '<option value="'.$timeZoneKey.'" '.$selected.'>'.$timeZoneLabel.'</option>';
-	}
-	?>  
+    $timezoneidentifier     =get_post_meta( $post->ID, '_wswebinar_timezoneidentifier', true );
+    $getTimezoneIdentifiers =WebinarSysteem::getTimezoneIdentifiers();
+    foreach ( $getTimezoneIdentifiers as $timeZoneKey => $timeZoneLabel ) {
+        $selected = ($timezoneidentifier==$timeZoneKey) ? 'selected="selected"' : '';
+        echo '<option value="' . $timeZoneKey . '" ' . $selected . '>' . $timeZoneLabel . '</option>';
+    }
+    ?>
     </select>
 </div>
 <div class="form-field">
     <?php
-    $_wswebinar_gener_duration = get_post_meta($post->ID, '_wswebinar_gener_duration', true);
-    if (empty($_wswebinar_gener_duration))
+    $_wswebinar_gener_duration = get_post_meta( $post->ID, '_wswebinar_gener_duration', true );
+    if ( empty( $_wswebinar_gener_duration ) ) {
         $_wswebinar_gener_duration = 3600;
-    $_wswebinar_gener_duration = floatval($_wswebinar_gener_duration);
+    }
+    $_wswebinar_gener_duration = floatval( $_wswebinar_gener_duration );
     ?>
     <label>Webinar Duration</label>
     <select name="gener_duration">
-        <?php for ($rr = 600; $rr < 18000; $rr+=600): ?>
-            <option value="<?php echo $rr; ?>" <?php echo $rr == $_wswebinar_gener_duration ? 'selected="selected"' : '' ?> ><?php echo $rr > 3590 ? date("H", $rr) . 'h' : ''; ?> <?php echo date("i", $rr); ?>min</option>
+        <?php for ( $rr = 600; $rr < 18000; $rr+=600 ) : ?>
+            <option value="<?php echo $rr; ?>" <?php echo $rr == $_wswebinar_gener_duration ? 'selected="selected"' : ''; ?> ><?php echo $rr > 3590 ? date( 'H', $rr ) . 'h' : ''; ?> <?php echo date( 'i', $rr ); ?>min</option>
 <?php endfor; ?>
     </select>
     <div class="webinar_clear_fix"></div>
@@ -89,8 +91,8 @@
 
 
 <div class="form-group">
-    <label for="gener_regdisabled_yn"><?php _e('Disable registration', WebinarSysteem::$lang_slug); ?></label>
-<?php $gener_regdisabled_yn_value = get_post_meta($post->ID, '_wswebinar_gener_regdisabled_yn', true); ?>
-    <input type="checkbox" data-switch="true" name="gener_regdisabled_yn" id="gener_regdisabled_yn" value="yes" <?php echo ($gener_regdisabled_yn_value == "yes" ) ? 'checked="checked"' : ''; ?> >
+    <label for="gener_regdisabled_yn"><?php _e( 'Disable registration', WebinarSysteem::$lang_slug ); ?></label>
+<?php $gener_regdisabled_yn_value = get_post_meta( $post->ID, '_wswebinar_gener_regdisabled_yn', true ); ?>
+    <input type="checkbox" data-switch="true" name="gener_regdisabled_yn" id="gener_regdisabled_yn" value="yes" <?php echo ($gener_regdisabled_yn_value == 'yes' ) ? 'checked="checked"' : ''; ?> >
     <div class="webinar_clear_fix"></div>
 </div>
