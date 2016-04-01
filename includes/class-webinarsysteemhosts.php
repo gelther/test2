971edd953ac1e19973ab47e423d794d69d2e6de6
gelther <lpw.sormano@gmail.com>
2016-04-01 10:49:01 +0200

    Code consistency changes for includes/class-webinarsysteemhosts.php

--- includes/class-webinarsysteemhosts.php
+++ includes/class-webinarsysteemhosts.php
@@ -2,21 +2,31 @@
 
 class WebinarSysteemHosts extends WebinarSysteem {
 
-    public static function getHostsArray($postID) {
-        $string = get_post_meta($postID, '_wswebinar_hostmetabox_hostname', true);
-        return $hosts = explode(',', $string);
+    public static function getHostsArray( $postID ) {
+
+        $string = get_post_meta( $postID, '_wswebinar_hostmetabox_hostname', true );
+        return $hosts = explode( ',', $string );
+
     }
 
-    public static function isMultipleHosts($postID) {
-        $hosts = self::getHostsArray($postID);
-        if (count($hosts) > 1)
-            return TRUE;
-        return FALSE;
+
+    public static function isMultipleHosts( $postID ) {
+
+        $hosts = self::getHostsArray( $postID );
+        if ( count( $hosts ) > 1 ) {
+            return true;
+        }
+        return false;
+
     }
 
-    public static function hostCount($postID) {
-        $hosts = self::getHostsArray($postID);
-        return count($hosts);
+
+    public static function hostCount( $postID ) {
+
+        $hosts = self::getHostsArray( $postID );
+        return count( $hosts );
+
     }
+
 
 }
