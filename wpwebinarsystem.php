205b3894cdabe08fcd9e552c37ddd52b389d742d
gelther <lpw.sormano@gmail.com>
2016-04-01 10:50:05 +0200

    Code consistency changes for wpwebinarsystem.php

--- wpwebinarsystem.php
+++ wpwebinarsystem.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
   Plugin Name: WP WebinarSystem
   Plugin URI: http://www.wpwebinarsystem.com
   Description: WP WebinarSystem allows you to run webinars within your Wordpress website, and customize everything around it.
@@ -14,4 +14,4 @@
 
 include 'includes/core-import.php';
 
-new WebinarSysteem(__FILE__);
+new WebinarSysteem( __FILE__ );
