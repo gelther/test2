11cc7644762380a9f38d8db59c4aa6822b279a3d
gelther <lpw.sormano@gmail.com>
2016-04-01 10:49:21 +0200

    Code consistency changes for includes/templates/template-email-admin-email.php

--- includes/templates/template-email-admin-email.php
+++ includes/templates/template-email-admin-email.php
@@ -2,13 +2,16 @@
 
 class WsWebinarTemplate_AdminDefault {
 
-    public static function get($post_id,$inputName,$inputEmail) {
+    public static function get( $post_id, $inputName, $inputEmail ) {
+
         ?>
-        <p><?php _e('Howdy', WebinarSysteem::$lang_slug) ?>,</p>
-        <p style="margin:25px 0px;"><?php echo $inputName; ?> (<?php echo $inputEmail; ?>) <?php _e('just signed up for your webinar', WebinarSysteem::$lang_slug) ?> <i><?php echo get_the_title($post_id) ?></i></p>
-        <p><?php _e('Regards', WebinarSysteem::$lang_slug) ?>,</p>
-        <p><i><?php echo get_bloginfo('name'); ?></i></p>
+        <p><?php _e( 'Howdy', WebinarSysteem::$lang_slug ); ?>,</p>
+        <p style="margin:25px 0px;"><?php echo $inputName; ?> (<?php echo $inputEmail; ?>) <?php _e( 'just signed up for your webinar', WebinarSysteem::$lang_slug ); ?> <i><?php echo get_the_title( $post_id ); ?></i></p>
+        <p><?php _e( 'Regards', WebinarSysteem::$lang_slug ); ?>,</p>
+        <p><i><?php echo get_bloginfo( 'name' ); ?></i></p>
         <?php
+
     }
+
 
 }
